edb189e9ad6db2f622545548c02c75d672737d75
Alexamakans <fougner.alexander@gmail.com>
2024-08-19 09:36:09 +0200

    add next call

--- middlewares/middlewares.js
+++ middlewares/middlewares.js
@@ -1,6 +1,7 @@
 module.exports = {
     testMode: function (req, res, next) {
         req.session.accountId = 0;
+        next();
     },
     requireLoggedIn: function (req, res, next) {
         if (
